2fb630f5d2ab889db5dcb22f7e5e19ab6e227ceb
@hexlet/cli <support@hexlet.io>
2023-08-21 00:20:41 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -5,9 +5,17 @@ import java.util.stream.Stream;
 
 public class App {
 
+    public static void main(String[] args) {
+//        String content = "environment=\"X_FORWARDED_MAIL=tirion@google.com,X_FORWARDED_HOME=/home/tirion,language=en\"";
+        String content = "[program:options]\nenvironment=\"X_FORWARDED_variable=value,  \"\n\nkey=value";
+        String result = getForwardedVariables(content);
+        System.out.println(result); // => "MAIL=tirion@google.com,HOME=/home/tirion,var3=value"
+    }
+
     public static String getForwardedVariables(String nameConfig) {
 
-        return Stream.of(nameConfig)
+        String[] linesArray = nameConfig.split("\n");
+        return Stream.of(linesArray)
                 .map(s -> s.trim().replaceAll("^environment=\"|\"$", ""))
                 .flatMap(s -> Stream.of(s.split(",")))
                 .filter(s -> s.startsWith("X_FORWARDED_"))

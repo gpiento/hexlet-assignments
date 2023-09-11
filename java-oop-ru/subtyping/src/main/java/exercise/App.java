67359fa916184690299e92fe795d52f263e88326
@hexlet/cli <support@hexlet.io>
2023-09-11 23:07:24 +0300

    submit java-oop-ru/subtyping

---
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -0,0 +1,28 @@
+package exercise;
+
+import java.util.Map;
+
+public class App {
+
+    public static void swapKeyValue(KeyValueStorage element) {
+        for (Map.Entry<String, String> entry : element.toMap().entrySet()) {
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        KeyValueStorage storage = new InMemoryKV(Map.of("key", "10"));
+        // Получение значения по ключу
+        System.out.println(storage.get("key", "default")); // "10"
+        System.out.println(storage.get("unknown", "default")); // "default"
+        // Установка нового значения
+        storage.set("key2", "value2");
+        System.out.println(storage.get("key2", "default")); // "value2"
+        // Удаление ключа
+        storage.unset("key2");
+        System.out.println(storage.get("key2", "default")); // "default"
+        // Получение всех данных из базы
+        Map<String, String> data = storage.toMap();
+        System.out.println(data); // => {key=10};
+    }
+}
